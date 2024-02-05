e398c3c2ff00ab3e3d07440538f2e5e51eeaa8c1
Subeom Kwon <subeomkwon@gmail.com>
2024-02-06 00:12:12 +0900

    기능 추가: Jsoup을 사용한 웹 스크래핑으로 금융 데이터 추출
    - Jsoup 라이브러리를 사용하여 Yahoo Finance에서 과거 배당금 정보를 스크래핑하는 기능 구현

--- src/main/java/com/example/dividend/DividendApplication.java
+++ src/main/java/com/example/dividend/DividendApplication.java
@@ -1,13 +1,43 @@
 package com.example.dividend;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.lang.annotation.Documented;
 
 @SpringBootApplication
 public class DividendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(DividendApplication.class, args);
+
+        Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/KO/history?period1=-252374400&period2" +
+                "=1707091200&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+        Document document = connection.get();
+        Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+        Element ele = eles.get(0);
+
+        Element tbody = ele.children().get(1);
+        for(Element e : tbody.children()){
+            String txt = e.text();
+            if(!txt.endsWith("Dividend")){
+                continue;
+            }
+            String[] splits = txt.split(" ");
+            String month = splits[0];
+            int day = Integer.valueOf(splits[1].replace(",", ""));
+            int year = Integer.valueOf(splits[2]);
+            String dividend = splits[3];
+
+            System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+        }
+
     }
 
 }
