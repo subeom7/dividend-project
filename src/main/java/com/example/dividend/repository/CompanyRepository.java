04bb9b9fb57a8ce455992d7c0bc7a023d7b7087f
Subeom Kwon <subeomkwon@gmail.com>
2024-02-07 01:22:24 +0900

    "CompanyRepository 인터페이스 추가: 주식 회사 데이터 접근 및 관리
    JpaRepository를 상속받아 CompanyEntity 관련 데이터 접근 및 CRUD 작업을 위한 인터페이스 구현
    Spring Data JPA를 사용하여 데이터베이스와의 상호작용을 추상화하고, 복잡한 데이터 접근 로직 없이도 필요한 데이터 작업을 수행할 수 있게 함
    existsByTicker 메서드 추가로 특정 티커를 가진 CompanyEntity의 존재 여부를 쉽게 확인할 수 있도록 지원. 이는 주식 티커가 유니크해야 하는 요구사항을 코드 상에서 효율적으로 처리하기 위함

---
+++ src/main/java/com/example/dividend/repository/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.example.dividend.repository;
+
+import com.example.dividend.domain.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+    boolean existsByTicker(String ticker);
+}
