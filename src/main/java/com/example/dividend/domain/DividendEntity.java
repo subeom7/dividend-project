292d64c13f7216185333d837cbc9ad320764777b
Subeom Kwon <subeomkwon@gmail.com>
2024-02-07 01:26:05 +0900

    "DividendEntity 도메인 모델 추가: 배당금 데이터를 위한 JPA 엔티티
    DIVIDEND 테이블과 매핑되는 DividendEntity 클래스를 통해 배당금 정보의 영속성 관리
    @Entity 애너테이션을 사용하여 JPA 엔티티임을 명시하고, 데이터베이스의 DIVIDEND 테이블에 매핑
    필드에는 id (기본 키), companyId (회사 ID를 참조하는 외래 키 역할), date (배당금 지급 날짜), dividend (배당금 액수) 포함
    @Id 및 @GeneratedValue(strategy = GenerationType.IDENTITY) 애너테이션을 사용하여 id 필드를 기본 키로 설정하고, 데이터베이스에서 자동으로 ID 값을 생성하도록 함
    companyId 필드를 통해 CompanyEntity와의 관계를 나타내며, 향후 JPA의 관계 매핑 기능을 이용하여 연관 관계를 설정할 수 있음
    LocalDateTime 타입의 date 필드를 사용하여 배당금 지급 날짜와 시간 정보를 정밀하게 관리
    Dividend DTO를 인자로 받는 생성자를 통해 DTO로부터 엔티티 객체를 쉽게 생성할 수 있도록 구현
    Lombok 라이브러리의 @Getter, @ToString, @NoArgsConstructor 애너테이션 사용으로 getter 메서드, toString() 메서드 및 기본 생성자 자동 생성

---
+++ src/main/java/com/example/dividend/domain/DividendEntity.java
@@ -0,0 +1,37 @@
+package com.example.dividend.domain;
+
+import com.example.dividend.dto.Dividend;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "DIVIDEND")
+@Getter
+@ToString
+@NoArgsConstructor
+public class DividendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long companyId;
+
+    private LocalDateTime date;
+
+    private String dividend;
+
+    public DividendEntity(Long companyId, Dividend dividend){
+        this.companyId = companyId;
+        this.date = dividend.getDate();
+        this.dividend = dividend.getDividend();
+    }
+
+}
