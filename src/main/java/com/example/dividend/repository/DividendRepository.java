3f71e138073dad19d81ae47dba47b7c3b7a2430a
Subeom Kwon <subeomkwon@gmail.com>
2024-02-07 01:32:23 +0900

    "DividendRepository 인터페이스 추가: 배당금 정보 접근 및 관리
    JpaRepository 인터페이스를 상속받아 DividendEntity에 대한 데이터 접근 및 CRUD 작업을 수행할 수 있는 DividendRepository 인터페이스 구현
    Spring Data JPA의 기능을 활용하여 복잡한 데이터베이스 연산 없이도 필요한 배당금 정보의 조회, 저장, 수정, 삭제 등의 작업을 간단하게 수행할 수 있음
    @Repository 애너테이션을 사용하여 이 인터페이스가 데이터 접근 계층의 컴포넌트임을 명시하고, 스프링 컨테이너에 의해 자동으로 관리되도록 설정

---
+++ src/main/java/com/example/dividend/repository/DividendRepository.java
@@ -0,0 +1,9 @@
+package com.example.dividend.repository;
+
+import com.example.dividend.domain.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+}
