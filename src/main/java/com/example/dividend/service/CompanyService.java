78d3dea725e498484385520a7d09dd5cc423f1e8
Subeom Kwon <subeomkwon@gmail.com>
2024-02-08 00:36:02 +0900

    autocomplete 기능 구현

--- src/main/java/com/example/dividend/service/CompanyService.java
+++ src/main/java/com/example/dividend/service/CompanyService.java
@@ -8,6 +8,11 @@ import com.example.dividend.repository.CompanyRepository;
 import com.example.dividend.repository.DividendRepository;
 import com.example.dividend.scraper.Scraper;
 import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.Trie;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -21,6 +26,7 @@ public class CompanyService {
     private final Scraper yahooFinanceScraper;
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
+    private final Trie trie;
 
     public Company save(String ticker) throws IOException {
         boolean exsists = companyRepository.existsByTicker(ticker);
@@ -28,6 +34,10 @@ public class CompanyService {
             throw new RuntimeException("The ticker already exsists -> " + ticker);
         }
         return storeCompanyAndDividend(ticker);
+    }
+
+    public Page<CompanyEntity> getAllCompany(Pageable pageable) {
+        return this.companyRepository.findAll(pageable);
     }
 
     private Company storeCompanyAndDividend(String ticker) throws IOException {
@@ -46,5 +56,27 @@ public class CompanyService {
 
         this.dividendRepository.saveAll(dividendEntityList);
         return company;
+    }
+
+    public List<String> getCompanyNamesByKeyword(String keyword){
+        Pageable limit = PageRequest.of(0, 10);
+        Page<CompanyEntity> companyEntities = this.companyRepository.findByNameStartingWithIgnoreCase(keyword, limit);
+        return companyEntities.stream()
+                .map(e -> e.getName())
+                .collect(Collectors.toList());
+    }
+
+
+    public void addAutoCompleteKeyword(String keyword){
+        this.trie.put(keyword, null);
+    }
+
+    public List<String> autocomplete(String keyword){
+        return (List<String>) this.trie.prefixMap(keyword).keySet()
+                .stream().collect(Collectors.toList());
+    }
+
+    public void deleteAutocompleteKeyword(String keyword){
+        this.trie.remove(keyword);
     }
 }
