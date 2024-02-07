cf96973ea56c0911e90701cec8e0440e77928475
Subeom Kwon <subeomkwon@gmail.com>
2024-02-08 00:34:06 +0900

    Dividend 엔티티에 companyId와 date 컬럼 유니크 제약 조건 추가

--- src/main/java/com/example/dividend/domain/DividendEntity.java
+++ src/main/java/com/example/dividend/domain/DividendEntity.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -16,6 +13,13 @@ import java.time.LocalDateTime;
 @Getter
 @ToString
 @NoArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = { "companyId", "date" }
+                )
+        }
+)
 public class DividendEntity {
 
     @Id
