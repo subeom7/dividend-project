c8d2bd572b8f48c223ce191419bab29513ec1db6
Subeom Kwon <subeomkwon@gmail.com>
2024-02-09 00:06:35 +0900

    Redis 설정 추가회사명을 기반으로 배당금 정보 캐시 사용 하여 조회 기능 추가

--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -1,5 +1,6 @@
 package com.example.dividend.service;
 
+import com.example.dividend.constants.CacheKey;
 import com.example.dividend.domain.CompanyEntity;
 import com.example.dividend.domain.DividendEntity;
 import com.example.dividend.dto.Company;
@@ -8,6 +9,8 @@ import com.example.dividend.dto.ScrapedResult;
 import com.example.dividend.repository.CompanyRepository;
 import com.example.dividend.repository.DividendRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -18,12 +21,16 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 public class FinanceService {
 
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
+
+    @Cacheable(key="#companyName", value = CacheKey.KEY_FINANCE)
     public ScrapedResult getDividendByCompanyName(String companyName){
+        log.info("search company ->" + companyName);
         //1. 회사명을 기준으로 회사 정보조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
                                         .orElseThrow(() -> new RuntimeException("존재 하지 않는 회사명 입니다"));
@@ -44,16 +51,11 @@ public class FinanceService {
 
         //stream 사용구현
         List<Dividend> dividends = dividendEntities.stream()
-                .map(e -> Dividend.builder()
-                        .date(e.getDate())
-                        .dividend(e.getDividend())
-                        .build())
-                .toList();
+                .map(e -> new Dividend(e.getDate(), e.getDividend()))
+                .collect(Collectors.toList());
 
-        return new ScrapedResult(Company.builder()
-                .ticker(company.getTicker())
-                .name(company.getName())
-                .build(), dividends);
+        return new ScrapedResult(new Company(company.getTicker(), company.getName()),
+                                dividends);
     }
 
 }
