050aaa964ebfbe6e36296d66e5cc6ee549c95f00
Subeom Kwon <subeomkwon@gmail.com>
2024-02-09 00:09:13 +0900

    ScraperScheduler 클래스에 캐싱 기능 추가 및 Company 객체 생성 방식을 빌더 패턴에서 직접 생성자 호출로 변경

--- src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
+++ src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
@@ -1,5 +1,6 @@
 package com.example.dividend.scheduler;
 
+import com.example.dividend.constants.CacheKey;
 import com.example.dividend.domain.CompanyEntity;
 import com.example.dividend.domain.DividendEntity;
 import com.example.dividend.dto.Company;
@@ -10,6 +11,8 @@ import com.example.dividend.scraper.Scraper;
 import com.example.dividend.scraper.YahooFinanceScraper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +22,7 @@ import java.util.List;
 @Slf4j
 @Component
 @AllArgsConstructor
+@EnableCaching
 public class ScraperScheduler {
 
     private final CompanyRepository companyRepository;
@@ -31,6 +35,7 @@ public class ScraperScheduler {
 //    }
 
     //일정 주기마다 수행
+    @CacheEvict(value= CacheKey.KEY_FINANCE, allEntries = true)
     @Scheduled(cron="${scheduler.scrap.yahoo}")
     public void yahooFinanceScheduling() throws IOException {
 
@@ -39,10 +44,9 @@ public class ScraperScheduler {
         //회사마다 배당금 정보를 새로 스크래핑
         for(CompanyEntity company : companies){
             log.info("scraping scheduler has started -> " + company.getName());
-            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(Company.builder()
-                            .name(company.getName())
-                            .ticker(company.getTicker())
-                            .build());
+            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(
+                    new Company(company.getTicker(), company.getName()));
+
             //스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
             scrapedResult.getDividends().stream()
                     //디비든 모델을 디비든 엔티티로 매칭
