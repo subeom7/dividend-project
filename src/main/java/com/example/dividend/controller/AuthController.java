9bd8149a85e86d35b677ba489b0c5340fb3839bd
Subeom Kwon <subeomkwon@gmail.com>
2024-02-10 22:35:23 +0900

    인증 컨트롤러 추가: 회원가입과 로그인 기능 구현

---
+++ src/main/java/com/example/dividend/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.example.dividend.controller;
+
+import com.example.dividend.domain.MemberEntity;
+import com.example.dividend.dto.Auth;
+import com.example.dividend.security.TokenProvider;
+import com.example.dividend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request){
+        MemberEntity result = this.memberService.register(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> signIn(@RequestBody Auth.SignIn request){
+        MemberEntity member = this.memberService.authenticate(request);
+        String token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+
+        return ResponseEntity.ok(token);
+    }
+}
