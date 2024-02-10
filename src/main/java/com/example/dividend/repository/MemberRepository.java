df9ddaf4fdc14fb7ebdf3db140331bf8ec3bbf51
Subeom Kwon <subeomkwon@gmail.com>
2024-02-10 22:39:27 +0900

    회원 리포지토리 구현: 사용자 이름으로 회원 조회 및 사용자 이름 존재 여부 확인 기능 추가

---
+++ src/main/java/com/example/dividend/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.example.dividend.repository;
+
+import com.example.dividend.domain.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+}
