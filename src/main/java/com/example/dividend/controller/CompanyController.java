bac74021daccdf3eebdef91e888d12d0b200fec3
Subeom Kwon <subeomkwon@gmail.com>
2024-02-10 22:36:37 +0900

    회사 관련 API에 대한 보안 권한 추가: 조회는 'READ' 권한, 추가는 'WRITE' 권한 필요

--- src/main/java/com/example/dividend/controller/CompanyController.java
+++ src/main/java/com/example/dividend/controller/CompanyController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,12 +32,20 @@ public class CompanyController {
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('READ')")
     public ResponseEntity<?> searchCompany(final Pageable pageable) {
         Page<CompanyEntity> companies = this.companyService.getAllCompany(pageable);
         return ResponseEntity.ok(companies);
     }
 
+    /**
+     * 회사 뱅당금 정보 추가
+     * @param request
+     * @return
+     * @throws IOException
+     */
     @PostMapping
+    @PreAuthorize("hasRole('WRITE')")
     public ResponseEntity<?> addCompany(@RequestBody Company request) throws IOException {
         String ticker = request.getTicker().trim();
         if(ObjectUtils.isEmpty(ticker)){
