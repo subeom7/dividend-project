3200d0e4c5958a32709fcd177184ab4d25811d7b
Subeom Kwon <subeomkwon@gmail.com>
2024-02-12 00:10:57 +0900

    전역 예외 처리기 구현: 사용자 정의 예외에 대한 처리 추가

---
+++ src/main/java/com/example/dividend/exception/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.dividend.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+@Slf4j
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(AbstractException.class)
+    protected ResponseEntity<ErrorResponse> handleCustomException(AbstractException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(e.getStatusCode())
+                .message(e.getMessage())
+                .build();
+                return new ResponseEntity<>(errorResponse, HttpStatus.resolve(e.getStatusCode()));
+    }
+}
